c7d0333b30dd283f67f527c5268edd7d0b073c17
Paulo <paulohfcmendes@gmail.com>
2021-01-28 11:37:58 -0300

    creating discipline entity

---
+++ src/entities/Discipline.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Course from './Course';
+
+@Entity('disciplines')
+class Discipline {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  hours: number;
+
+  @ManyToOne(() => Course)
+  @JoinColumn({ name: 'provider_id' })
+  course: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Discipline;
